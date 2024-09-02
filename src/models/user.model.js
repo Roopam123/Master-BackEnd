7472e2be800e5684d8d537ec94910cdffabc0b50
Roopam <roopamr2411@gmail.com>
2024-09-03 01:27:59 +0530

    add multer and clodinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,7 +60,7 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCurrect = async function (password) {
   // for compare the password
-  await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 // generate access token
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const fileUploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const cloudinaryRespons = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // successfully file upload in cloudinary
+    console.log(
+      "successfully file upload in cloudinary",
+      cloudinaryRespons.url
+    );
+    return cloudinaryRespons;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the local saved temporary as the upload operation got faild
+    console.log("Error on the fileUploadOnCloudinary function", error);
+  }
+};
+
+export { fileUploadOnCloudinary };
